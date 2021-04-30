6e651713572a67051481c1e4c2806de00070114a
mengjun.deng <mengjun.deng@mybogames.com>
2021-04-30 19:11:05 +0800

    4-30学习

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
-const { EXDEV } = require('node:constants')
-const path = require('path')
-module.exports ={
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+module.exports = {
   //入口文件
-  entry:"./main.js",
-  output:{    
-
-      filename:'bundle.js',
-      path:path.resolve(__dirname,'./dist')
+  entry: "./main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
   },
-  module:{
-    rules:[{
-      test:/\.css$/,
-      use:['style-loader','css-loader']
-    }]
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
   },
-  mode:'development',
-  // plugins :[
-  //   new ExtractTextPlugin({
-  //     filename:`[name]_{contenthash:8}.css`
-  //   }),
-  // ]
-}
+  mode: "development",
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
